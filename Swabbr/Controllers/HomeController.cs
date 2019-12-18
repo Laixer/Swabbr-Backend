3afb2af65bb385999ddee3151d5fe70acf193728
Beau <31695264+Beau@users.noreply.github.com>
2019-12-18 13:44:27 +0100

    Create HomeController.cs

---
+++ Swabbr/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Swabbr.Api.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet("/")]
+        public async Task<IActionResult> Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
