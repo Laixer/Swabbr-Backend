570b619abf4d2157a3d79f80690d2526ffde0124
Beau <31695264+Beau@users.noreply.github.com>
2020-01-21 15:52:46 +0100

    XML comments for statistics output model

--- Swabbr/ViewModels/User/UserStatisticsOutputModel.cs
+++ Swabbr/ViewModels/User/UserStatisticsOutputModel.cs
@@ -2,12 +2,39 @@
 {
     public class UserStatisticsOutputModel
     {
+        /// <summary>
+        /// The total amount of accumulated likes for the vlogs of this user.
+        /// </summary>
         public int TotalLikes { get; set; }
+
+        /// <summary>
+        /// The total amount of users that are following this user.
+        /// </summary>
         public int TotalFollowers { get; set; }
+
+        /// <summary>
+        /// The total amount of users this user is following.
+        /// </summary>
         public int TotalFollowing { get; set; }
+
+        /// <summary>
+        /// The total amount of placed reactions by this user.
+        /// </summary>
         public int TotalReactionsGiven { get; set; }
+
+        /// <summary>
+        /// The total amount of received reactions by this user.
+        /// </summary>
         public int TotalReactionsReceived { get; set; }
+        
+        /// <summary>
+        /// The total amount of placed vlogs by this user.
+        /// </summary>
         public int TotalVlogs { get; set; }
+
+        /// <summary>
+        /// The total amount of accumulated views for the vlogs this user owns.
+        /// </summary>
         public int TotalViews { get; set; }
     }
 }
