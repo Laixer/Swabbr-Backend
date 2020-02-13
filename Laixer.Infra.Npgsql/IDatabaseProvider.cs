420d9a6c957ebc7bb8f1b88ac0f06d9242fccb22
tabeckers <tabeckers95@gmail.com>
2020-02-13 14:06:13 +0100

    Added new reusable library for providing database connections

---
+++ Laixer.Infra.Npgsql/IDatabaseProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Laixer.Infra.Npgsql
+{
+
+    /// <summary>
+    /// Provides 
+    /// </summary>
+    public interface IDatabaseProvider
+    {
+
+        /// <summary>
+        /// Gets a database connection scope.
+        /// </summary>
+        /// <returns><see cref="IDbConnection"/></returns>
+        IDbConnection GetConnectionScope();
+
+    }
+
+}
---
+++ Laixer.Infra.Npgsql/NpgsqlDatabaseProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Npgsql;
+using System;
+using System.Data;
+
+namespace Laixer.Infra.Npgsql
+{
+
+    /// <summary>
+    /// Used to provide connections to a postgresql database.
+    /// </summary>
+    public class NpgsqlDatabaseProvider : IDatabaseProvider
+    {
+
+        /// <summary>
+        /// Contains our connection string.
+        /// </summary>
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Constructor for dependency injection.
+        /// </summary>
+        public NpgsqlDatabaseProvider(IOptions<NpgsqlDatabaseProviderOptions> options,
+            IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            if (configuration == null) { throw new ArgumentNullException(nameof(configuration)); }
+            if (options.Value == null) { throw new ArgumentNullException(nameof(options.Value)); }
+            if (string.IsNullOrEmpty(options.Value.ConnectionStringName)) { throw new InvalidOperationException(nameof(options.Value.ConnectionStringName)); }
+
+            connectionString = configuration.GetConnectionString(options.Value.ConnectionStringName);
+            if (string.IsNullOrEmpty(connectionString)) { throw new InvalidOperationException($"IConfiguration does not contains connection string with name {options.Value.ConnectionStringName}"); }
+        }
+
+        /// <summary>
+        /// Gets a connection scope.
+        /// </summary>
+        /// <returns><see cref="NpgsqlConnection"/></returns>
+        public IDbConnection GetConnectionScope() => new NpgsqlConnection(connectionString);
+
+    }
+
+}
