6b9b3bd5f6306aadb4fdbd5b380fd948ca876b41
tabeckers <tabeckers95@gmail.com>
2020-02-27 16:20:53 +0100

    Added services for vlog triggers and user livestream requests

---
+++ Swabbr.Core/Services/UserStreamingHandlingService.cs
@@ -0,0 +1,118 @@
+﻿using Laixer.Utility.Extensions;
+using Swabbr.Core.Entities;
+using Swabbr.Core.Interfaces.Repositories;
+using Swabbr.Core.Interfaces.Services;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Swabbr.Core.Services
+{
+
+    /// <summary>
+    /// Contains functionality to handle user streaming requests.
+    /// </summary>
+    public sealed class UserStreamingHandlingService : IUserStreamingHandlingService
+    {
+
+        private readonly ILivestreamingService _livestreamingService;
+        private readonly INotificationService _notificationService;
+
+        private readonly IUserRepository _userRepository;
+        private readonly ILivestreamRepository _livestreamRepository;
+        private readonly IVlogRepository _vlogRepository;
+
+        /// <summary>
+        /// Constructor for dependency injection.
+        /// </summary>
+        public UserStreamingHandlingService(ILivestreamingService livestreamingService,
+            INotificationService notificationService,
+            IUserRepository userRepository,
+            ILivestreamRepository livestreamRepository,
+            IVlogRepository vlogRepository)
+        {
+            _livestreamingService = livestreamingService ?? throw new ArgumentNullException(nameof(livestreamingService));
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _livestreamRepository = livestreamRepository ?? throw new ArgumentNullException(nameof(livestreamRepository));
+            _vlogRepository = vlogRepository ?? throw new ArgumentNullException(nameof(vlogRepository));
+        }
+
+        /// <summary>
+        /// Called when the user starts streaming to a <see cref="Livestream"/>.
+        /// This creates a <see cref="Vlog"/> to bind to the stream.
+        /// </summary>
+        /// <param name="userId">Internal <see cref="SwabbrUser"/> id</param>
+        /// <param name="livestreamId">Internal <see cref="Livestream"/> id</param>
+        /// <returns>The created <see cref="Vlog"/></returns>
+        public async Task<Vlog> OnUserStartStreaming(Guid userId, Guid livestreamId)
+        {
+            userId.ThrowIfNullOrEmpty();
+            livestreamId.ThrowIfNullOrEmpty();
+
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var user = await _userRepository.GetAsync(userId).ConfigureAwait(false);
+                var livestream = await _livestreamRepository.GetAsync(livestreamId).ConfigureAwait(false);
+
+                // Throw if the user doesn't own the livestream
+                if (livestream.UserId != user.Id) { throw new InvalidOperationException("User doesn't own this livestream"); }
+
+                // Throw if a vlog already exists for this livestream
+                if (await _vlogRepository.ExistsForLivestreamAsync(livestreamId).ConfigureAwait(false)) { throw new InvalidOperationException("This livestream already contains a vlog."); }
+
+                // Create a new vlog for the livestream
+                var vlog = await _vlogRepository.CreateAsync(new Vlog
+                {
+                    UserId = user.Id,
+                    LivestreamId = livestreamId,
+                }).ConfigureAwait(false);
+
+                // Process livestream itself
+                // This throws if the livestream isn't pending user
+                await _livestreamingService.OnUserStartStreamingAsync(livestream.Id, user.Id).ConfigureAwait(false);
+
+                // Notify all followers
+                await _notificationService.SendNotificationToFollowersAsync(user.Id, vlog.Id).ConfigureAwait(false);
+
+                // Commit and return
+                scope.Complete();
+                return vlog;
+            }
+        }
+
+        /// <summary>
+        /// Gets called when the user stops streaming.
+        /// </summary>
+        /// <param name="userId">Internal <see cref="SwabbrUser"/> id</param>
+        /// <param name="livestreamId">Internal <see cref="Livestream"/> id</param>
+        /// <returns><see cref="Task"/></returns>
+        public async Task OnUserStopStreaming(Guid userId, Guid livestreamId)
+        {
+            userId.ThrowIfNullOrEmpty();
+            livestreamId.ThrowIfNullOrEmpty();
+
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var user = await _userRepository.GetAsync(userId).ConfigureAwait(false);
+                var livestream = await _livestreamRepository.GetAsync(livestreamId).ConfigureAwait(false);
+
+                // Throw if the user doesn't own the livestream
+                if (livestream.UserId != user.Id) { throw new InvalidOperationException("User doesn't own this livestream"); }
+
+                // Stop the livestream
+                // This throws if the livestream isn't live
+                // This throws if the user isn't actually connected at first
+                await _livestreamingService.OnUserStopStreamingAsync(livestream.Id, userId).ConfigureAwait(false);
+
+                // Notify all followers
+                var vlog = await _vlogRepository.GetVlogFromLivestreamAsync(livestream.Id).ConfigureAwait(false);
+                await _notificationService.SendNotificationToFollowersAsync(user.Id, vlog.Id).ConfigureAwait(false);
+
+                scope.Complete();
+            }
+        }
+
+    }
+
+}
---
+++ Swabbr.Core/Services/VlogTriggerService.cs
@@ -0,0 +1,75 @@
+﻿using Laixer.Utility.Extensions;
+using Swabbr.Core.Interfaces.Repositories;
+using Swabbr.Core.Interfaces.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Swabbr.Core.Services
+{
+
+    /// <summary>
+    /// Handles the triggers where a <see cref="SwabbrUser"/> has to stat vlogging.
+    /// </summary>
+    public sealed class VlogTriggerService : IVlogTriggerService
+    {
+
+        private readonly ILivestreamingService _livestreamingService;
+        private readonly IUserRepository _userRepository;
+        private readonly INotificationService _notificationService;
+
+        /// <summary>
+        /// Constructor for dependency injection.
+        /// </summary>
+        public VlogTriggerService(ILivestreamingService livestreamingService,
+            IUserRepository userRepository,
+            INotificationService notificationService)
+        {
+            _livestreamingService = livestreamingService ?? throw new ArgumentNullException(nameof(livestreamingService));
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+        }
+
+        /// <summary>
+        /// Process the external trigger that a <see cref="SwabbrUser"/> has to start
+        /// vlogging.
+        /// </summary>
+        /// <param name="userId">Internal <see cref="SwabbrUser"/> id</param>
+        /// <returns><see cref="Task"/></returns>
+        public async Task ProcessVlogTriggerForUserAsync(Guid userId)
+        {
+            userId.ThrowIfNullOrEmpty();
+            if (!await _userRepository.UserExistsAsync(userId).ConfigureAwait(false)) { throw new InvalidOperationException("User doesn't exist"); }
+
+            var livestream = await _livestreamingService.CreateAndStartLivestreamForUserAsync(userId).ConfigureAwait(false);
+
+            TriggerUserTimeoutFunction(); // TODO HOW?
+            TriggerLivestreamTimeoutFunction(); // TODO HOW?
+
+            await _notificationService.SendVlogTriggerToUserAsync(userId, livestream.Id).ConfigureAwait(false);
+        }
+
+        public Task ProcessVlogTriggerTimoutAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This serves as a timeout in case the user never responds.
+        /// </summary>
+        private void TriggerUserTimeoutFunction()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// This serves as a timeout in case the vlog goes on for too long.
+        /// TODO Do we even need this?
+        /// </summary>
+        private void TriggerLivestreamTimeoutFunction()
+        {
+            return;
+        }
+
+    }
+
+}
