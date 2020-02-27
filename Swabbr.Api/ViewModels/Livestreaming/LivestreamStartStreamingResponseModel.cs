9cc2651555d568e53e4907b55f1a923a2ee6ed13
tabeckers <tabeckers95@gmail.com>
2020-02-27 16:23:00 +0100

    Created livestream start response viewmodel

---
+++ Swabbr.Api/ViewModels/Livestreaming/LivestreamStartStreamingResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Swabbr.Api.ViewModels.Livestreaming
+{
+
+    /// <summary>
+    /// The response object after we call <see cref="Controllers.LivestreamsController.StartStreamingAsync(Guid)"/>.
+    /// </summary>
+    public sealed class LivestreamStartStreamingResponseModel
+    {
+
+        /// <summary>
+        /// References the <see cref="Core.Entities.Vlog"/> that belongs 
+        /// to the current <see cref="Core.Entities.Livestream"/>.
+        /// </summary>
+        public Guid VlogId { get; set; }
+
+    }
+
+}
