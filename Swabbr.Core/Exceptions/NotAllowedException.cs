5b5965dd10fd84b2aaac9e8f1d5b7d9cef6dd8d3
tabeckers <tabeckers95@gmail.com>
2020-03-02 14:09:23 +0100

    Created new custom exceptions

---
+++ Swabbr.Core/Exceptions/NotAllowedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Swabbr.Core.Exceptions
+{
+
+    /// <summary>
+    /// <see cref="Exception"/> indicating we are not allowed to perform a certain
+    /// action in our backend.
+    /// </summary>
+    public sealed class NotAllowedException : Exception
+    {
+
+        public NotAllowedException(string message) : base(message) { }
+
+        public NotAllowedException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+
+}
---
+++ Swabbr.Core/Exceptions/OperationAlreadyExecutedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Swabbr.Core.Exceptions
+{
+
+    /// <summary>
+    /// <see cref="Exception"/> indicating that some operation was already performed
+    /// before, thus the expected output state is already the current state.
+    /// </summary>
+    public sealed class OperationAlreadyExecutedException : Exception
+    {
+
+        public OperationAlreadyExecutedException(string message) : base(message) { }
+
+        public OperationAlreadyExecutedException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+
+}
