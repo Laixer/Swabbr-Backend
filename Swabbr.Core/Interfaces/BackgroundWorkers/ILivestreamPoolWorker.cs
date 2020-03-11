60ef4acfa007ef142e71b01427f1f7155835cea8
tabeckers <tabeckers95@gmail.com>
2020-03-11 15:35:06 +0100

    Created background worker abstraction

---
+++ Swabbr.Core/Interfaces/BackgroundWorkers/ILivestreamPoolWorker.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Swabbr.Core.Interfaces.BackgroundWorkers
+{
+
+    /// <summary>
+    /// Contains functionality to control a <see cref="Entities.Livestream"/>
+    /// pool.
+    /// </summary>
+    public interface ILivestreamPoolWorker
+    {
+
+        Task UpdatePoolAsync();
+
+    }
+
+}
