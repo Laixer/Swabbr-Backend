9ce6b7b6e1141ecfc8ada0e54d9c5d58b1692e21
tabeckers <tabeckers95@gmail.com>
2020-03-23 12:30:23 +0100

    Created new exceptions for core

---
+++ Swabbr.Core/Exceptions/MultipleEntitiesFoundException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Swabbr.Core.Exceptions
+{
+
+    /// <summary>
+    /// Indicates we found more than one <see cref="Entities.EntityBase{TPrimary}"/> 
+    /// in our data store, where we should only find one.
+    /// </summary>
+    public sealed class MultipleEntitiesFoundException : Exception
+    {
+        public MultipleEntitiesFoundException(string message) : base(message)
+        {
+        }
+
+        public MultipleEntitiesFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public MultipleEntitiesFoundException()
+        {
+        }
+
+        protected MultipleEntitiesFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+    }
+
+}
