8498b9550a7fba3f78d2d6da7b761827431fcf45
tabeckers <tabeckers95@gmail.com>
2020-03-23 12:32:35 +0100

    Created SQL utility

---
+++ Swabbr.Infrastructure/Utility/SqlUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Swabbr.Infrastructure.Utility
+{
+
+    /// <summary>
+    /// Contains utility functions for SQL.
+    /// </summary>
+    internal static class SqlUtility
+    {
+
+        /// <summary>
+        /// Formats a <see cref="DateTimeOffset"/> to SQL format.
+        /// </summary>
+        /// <param name="date"><see cref="DateTimeOffset"/></param>
+        /// <returns>SQL formatted string</returns>
+        internal static string FormatDateTime(DateTimeOffset date)
+        {
+            if (date == null) { throw new ArgumentNullException(nameof(date)); }
+            return date.ToString("yyyy-MM-dd HH:mm:ss"); // TODO Check this
+        }
+
+    }
+
+}
