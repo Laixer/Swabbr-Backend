5859a876d727bb813fb35f095c76e22c45df1a39
tabeckers <tabeckers95@gmail.com>
2020-04-14 16:16:47 +0200

    Update DebugController.cs

--- Swabbr.Api/Controllers/DebugController.cs
+++ Swabbr.Api/Controllers/DebugController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Net.Http.Headers;
 using Swabbr.Api.Authentication;
 using Swabbr.Api.Utility;
+using Swabbr.AzureMediaServices.Services;
 using Swabbr.Core.Enums;
 using Swabbr.Core.Interfaces.Repositories;
 using Swabbr.Core.Interfaces.Services;
@@ -32,34 +33,43 @@ namespace Swabbr.Api.Controllers
         private readonly UserManager<SwabbrIdentityUser> _userManager;
         private readonly INotificationService _notificationService;
         private readonly ILivestreamPlaybackService _livestreamPlaybackService;
+        private readonly ILivestreamPoolService _livestreamPoolService;
+        private readonly ILivestreamService _livestreamService;
         private readonly IDeviceRegistrationService _deviceRegistrationService;
         private readonly ITranscodingService _transcodingService;
         private readonly IReactionUploadService _reactionUploadService;
         private readonly IStorageService _storageService;
         private readonly IReactionRepository _reactionRepository;
+        private readonly AMSDebugService _amsDebugService;
 
         /// <summary>
         /// Constructor for dependency injection.
         /// </summary>
-        public DebugController(IVlogTriggerService vlogTriggerService,
-            UserManager<SwabbrIdentityUser> userManager,
+        public DebugController(UserManager<SwabbrIdentityUser> userManager,
+            IVlogTriggerService vlogTriggerService,
             INotificationService notificationService,
+            ILivestreamPoolService livestreamPoolService,
             ILivestreamPlaybackService livestreamPlaybackService,
+            ILivestreamService livestreamService,
             IDeviceRegistrationService deviceRegistrationService,
             ITranscodingService transcodingService,
             IReactionUploadService reactionUploadService,
             IStorageService storageService,
-            IReactionRepository reactionRepository)
+            IReactionRepository reactionRepository,
+            AMSDebugService debugService)
         {
             _vlogTriggerService = vlogTriggerService ?? throw new ArgumentNullException(nameof(vlogTriggerService));
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+            _livestreamPoolService = livestreamPoolService ?? throw new ArgumentNullException(nameof(livestreamPoolService));
             _livestreamPlaybackService = livestreamPlaybackService ?? throw new ArgumentNullException(nameof(livestreamPlaybackService));
+            _livestreamService = livestreamService ?? throw new ArgumentNullException(nameof(livestreamService));
             _deviceRegistrationService = deviceRegistrationService ?? throw new ArgumentNullException(nameof(deviceRegistrationService));
             _transcodingService = transcodingService ?? throw new ArgumentNullException(nameof(transcodingService));
             _reactionUploadService = reactionUploadService ?? throw new ArgumentNullException(nameof(reactionUploadService));
             _storageService = storageService ?? throw new ArgumentNullException(nameof(storageService));
             _reactionRepository = reactionRepository ?? throw new ArgumentNullException(nameof(reactionRepository));
+            _amsDebugService = debugService ?? throw new ArgumentNullException(nameof(debugService));
         }
 
         [HttpGet("fastly_token_test")]
@@ -163,6 +173,21 @@ namespace Swabbr.Api.Controllers
                 return Ok(length);
                 // await _reactionUploadService.OnFinishedTranscodingReactionAsync(reactionId).ConfigureAwait(false);
                 // return Ok();
+            }
+            catch (Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpPost("ams_debug")]
+        public async Task<IActionResult> AmsDebug(Guid livestreamId)
+        {
+            try
+            {
+                var user = await _userManager.GetUserAsync(User).ConfigureAwait(false);
+                var livestream = await _livestreamPoolService.TryGetLivestreamFromPoolAsync().ConfigureAwait(false);
+                return Ok(livestream);
             }
             catch (Exception e)
             {
