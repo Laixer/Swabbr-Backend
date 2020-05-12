987c5bfa11c2145f6a437bf1765a20ce7e6ac954
tabeckers <tabeckers95@gmail.com>
2020-05-12 12:34:58 +0200

    Updated core types

---
+++ Swabbr.Core/Types/UserStartStreamingWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Swabbr.Core.Types
+{
+
+    /// <summary>
+    /// JSON wrapper for a user-start-streaming request.
+    /// </summary>
+    public sealed class UserStartStreamingWrapper
+    {
+
+        public Guid UserId { get; set; }
+
+        public Guid LivestreamId { get; set; }
+
+        public int UserConnectTimeoutSeconds { get; set; }
+
+    }
+
+}
