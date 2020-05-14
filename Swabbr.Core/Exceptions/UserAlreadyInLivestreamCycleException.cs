519d69b1c5e88195f91265df1511992fc8778659
tabeckers <tabeckers95@gmail.com>
2020-05-14 16:12:40 +0200

    Added new exceptions

---
+++ Swabbr.Core/Exceptions/UserAlreadyInLivestreamCycleException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Swabbr.Core.Exceptions
+{
+
+    /// <summary>
+    /// Represents the situation where a <see cref="Entities.SwabbrUser"/> is
+    /// in a livestreaming cycle when a second one is started.
+    /// </summary>
+    public sealed class UserAlreadyInLivestreamCycleException : Exception
+    {
+
+        public UserAlreadyInLivestreamCycleException() { }
+
+        public UserAlreadyInLivestreamCycleException(string message)
+            : base(message) { }
+
+        public UserAlreadyInLivestreamCycleException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+    }
+
+}
