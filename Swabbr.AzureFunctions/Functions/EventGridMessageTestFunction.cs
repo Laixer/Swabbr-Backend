4d03d425997fd4e857163b130ef4e7fd1dc991e9
tabeckers <tabeckers95@gmail.com>
2020-06-02 16:30:09 +0200

    Create EventGridMessageTestFunction.cs

---
+++ Swabbr.AzureFunctions/Functions/EventGridMessageTestFunction.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Swabbr.AzureFunctions.Functions
+{
+    /// <summary>
+    /// TODO Remove
+    /// </summary>
+    public sealed class EventGridMessageTestFunction
+    {
+        /// <summary>
+        /// Trigger when a user connects to a Live Event.
+        /// </summary>
+        /// <param name="eventGridEvent"><see cref="EventGridEvent"/></param>
+        /// <param name="log"><see cref="ILogger"/></param>
+        /// <returns><see cref="Task"/></returns>
+        [FunctionName(nameof(EncoderDisconnectedHttpFunction))]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            if (req == null) { throw new ArgumentNullException(nameof(req)); }
+            if (log == null) { throw new ArgumentNullException(nameof(log)); }
+
+            using var streamReader = new StreamReader(req.Body);
+            var body = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+
+            log.LogInformation($"Body:\n{body}");
+
+            return new OkObjectResult(body);
+        }
+    }
+}
