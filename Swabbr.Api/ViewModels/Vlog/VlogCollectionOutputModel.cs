4d0d30b57587661ab553d82e11afb66e562cc9b0
Thomas Beckers <tabeckers95@gmail.com>
2020-09-08 15:46:45 +0200

    Implemented vlog like summary

--- Swabbr.Api/ViewModels/Vlog/VlogCollectionOutputModel.cs
+++ Swabbr.Api/ViewModels/Vlog/VlogCollectionOutputModel.cs
@@ -3,17 +3,19 @@ using System.Linq;
 
 namespace Swabbr.Api.ViewModels.Vlog
 {
-
     /// <summary>
-    /// Wrapper class for a collection of <see cref="VlogOutputModel"/>s.
+    ///     Wrapper class for a collection of <see cref="VlogWithMetadataOutputModel"/>s.
     /// </summary>
     public sealed class VlogCollectionOutputModel
     {
-
+        /// <summary>
+        ///     Total vlogs in this output model.
+        /// </summary>
         public int VlogsCount => Vlogs.Count();
 
-        public IEnumerable<VlogOutputModel> Vlogs { get; set; }
-
+        /// <summary>
+        ///     Collection of vlogs with their metadata.
+        /// </summary>
+        public IEnumerable<VlogWithMetadataOutputModel> Vlogs { get; set; }
     }
-
 }
