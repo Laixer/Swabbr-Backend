78e1a094fff514eeda425ed24dbe5d13d6361574
Thomas Beckers <tabeckers95@gmail.com>
2020-12-03 17:42:08 +0100

    Update GuidExtensions.cs

--- Swabbr.Core/Extensions/GuidExtensions.cs
+++ Swabbr.Core/Extensions/GuidExtensions.cs
@@ -9,12 +9,6 @@ namespace Swabbr.Core.Extensions
     public static class GuidExtensions
     {
         /// <summary>
-        ///     Indicates how long a <see cref="Guid"/> is when parsed to <see cref="string"/>,
-        ///     according to the 00000000-0000-0000-0000-000000000000 format.
-        /// </summary>
-        public const int GuidAsStringLength = 36;
-
-        /// <summary>
         ///     Throws an <see cref="ArgumentNullException"/>
         ///     if the guid is equal to <see cref="Guid.Empty"/>.
         /// </summary>
