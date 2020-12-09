a1f86af0e8f5094cc04b5ea017d690a229f91f04
Thomas Beckers <tabeckers95@gmail.com>
2020-12-09 13:30:23 +0100

    Created data annotations

--- Swabbr.Core/DataAnnotations/NonEmptyStringAttribute.cs
+++ Swabbr.Core/DataAnnotations/NonEmptyStringAttribute.cs
@@ -1,21 +1,20 @@
-﻿using Swabbr.Core.Helpers;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Swabbr.Core.DataAnnotations
 {
     /// <summary>
-    ///     Attribute validating that some string is base 64 encoded.
+    ///     Attribute forcing a string to be not empty.
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false)]
-    public class Base64EncodedAttribute : ValidationAttribute
+    public class NonEmptyStringAttribute : ValidationAttribute
     {
         /// <summary>
-        ///     Checks if the value is null or base 64 encoded.
+        ///     Checks if the value is null or a non-empty string.
         /// </summary>
         /// <param name="value">The item to check.</param>
         public override bool IsValid(object value)
-            => value is null || (value is string str && Base64EncodedHelper.IsBase64Encoded(str));
+            => value is null || (value is string str && str.Length > 0);
 
         /// <summary>
         ///     Formats the error message when validation fails.
@@ -23,6 +22,6 @@ namespace Swabbr.Core.DataAnnotations
         /// <param name="name">Field name.</param>
         /// <returns>Formatted error message.</returns>
         public override string FormatErrorMessage(string name)
-            => $"The {name} field is not a valid base 64 encoded string.";
+            => $"The {name} field can't be an empty string.";
     }
 }
