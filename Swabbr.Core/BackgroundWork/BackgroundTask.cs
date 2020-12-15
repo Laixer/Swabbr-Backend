fd522fb38916be8df01e100cdf6d1e94ea06c1ee
Thomas Beckers <tabeckers95@gmail.com>
2020-12-15 13:20:40 +0100

    Created background work core package

---
+++ Swabbr.Core/BackgroundWork/BackgroundTask.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace Swabbr.Core.BackgroundWork
+{
+    /// <summary>
+    ///     Base for an executable background task.
+    /// </summary>
+    /// <remarks>
+    ///     Dependency injection can be used when 
+    ///     implementing this class.
+    /// </remarks>
+    public abstract class BackgroundTask
+    {
+        /// <summary>
+        ///     Determines if this task is capable of 
+        ///     processing a given object.
+        /// </summary>
+        /// <param name="value">The object to check.</param>
+        public abstract bool CanHandle(object value);
+        
+        /// <summary>
+        ///     Represents the work to be executed by this task.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public virtual async Task ExecuteAsync(BackgroundTaskContext context)
+        {
+            // Always yield for the state machine.
+            await Task.Yield();
+        }
+    }
+}
---
+++ Swabbr.Core/BackgroundWork/BackgroundTasksServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Swabbr.Core.BackgroundWork
+{
+    /// <summary>
+    ///     Contains extension functionality to add background
+    ///     tasks to the service collection.
+    /// </summary>
+    public static class BackgroundTasksServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Add a background task to the service collection.
+        /// </summary>
+        /// <typeparam name="TBackgroundTask">Task type.</typeparam>
+        /// <param name="services">Service collection.</param>
+        /// <returns>Service collection with background task added.</returns>
+        public static IServiceCollection AddBackgroundTask<TBackgroundTask>(this IServiceCollection services)
+            where TBackgroundTask : BackgroundTask
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddTransient(typeof(TBackgroundTask));
+            services.TryAddEnumerable(ServiceDescriptor.Transient<BackgroundTask, TBackgroundTask>());
+
+            return services;
+        }
+    }
+}
