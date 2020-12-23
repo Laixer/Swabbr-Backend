d0dd5a8b7f7921b82a33d66b82784a55c9239914
Thomas Beckers <tabeckers95@gmail.com>
2020-12-23 14:52:21 +0100

    Can now add multiple views to multiple vlogs

---
+++ Swabbr.Core/Context/AddVlogViewsContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swabbr.Core.Context
+{
+    /// <summary>
+    ///     Context for adding views to a vlog.
+    /// </summary>
+    public class AddVlogViewsContext
+    {
+        /// <summary>
+        ///     Id of the user that watched the vlogs.
+        /// </summary>
+        public Guid WatchingUserId { get; set; }
+
+        /// <summary>
+        ///     Dictionary containing the amount of views for each vlog.
+        /// </summary>
+        public Dictionary<Guid, uint> VlogViewPairs = new Dictionary<Guid, uint>();
+
+        /// <summary>
+        ///     Adds a given amount of views to a vlog id.
+        /// </summary>
+        /// <param name="vlogId">The vlog id</param>
+        /// <param name="views">The amount of views.</param>
+        public void AddForVlog(Guid vlogId, uint views)
+        {
+            if (vlogId == Guid.Empty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (VlogViewPairs.ContainsKey(vlogId))
+            {
+                var currentViews = VlogViewPairs[vlogId];
+
+                VlogViewPairs.Remove(vlogId);
+
+                VlogViewPairs.Add(vlogId, currentViews + views);
+            }
+            else
+            {
+                VlogViewPairs.Add(vlogId, views);
+            }
+        }
+    }
+}
