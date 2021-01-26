33ee73d9d43d27d063c026ed9645f92a27a4b06f
Thomas Beckers <tabeckers95@gmail.com>
2021-01-26 15:27:21 +0100

    Repaired notification serialization, switching back to Newtonsoft.Json
    The System.Text.Json library can't handle polymorphic serialization yet.

---
+++ Swabbr.Infrastructure/Helpers/KnownTypesBinder.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swabbr.Infrastructure.Helpers
+{
+    // FUTURE Decouple from Newtonsoft.Json when System.Text.Json can handle polymorphic serialization.
+    /// <summary>
+    ///     Newtonsoft Json helper class.
+    /// </summary>
+    public class KnownTypesBinder : ISerializationBinder
+    {
+        public IList<Type> KnownTypes { get; set; }
+
+        public Type BindToType(string assemblyName, string typeName) => KnownTypes.SingleOrDefault(t => t.Name == typeName);
+
+        public void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            assemblyName = null;
+            typeName = serializedType.Name;
+        }
+    }
+}
