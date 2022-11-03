01aa6b4740d205b408aea490834148829def3b48
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-11-04 00:02:31 +0100

    Code cleanup

--- Swabbr.Infrastructure/Notifications/JsonWrappers/FcmContentWrapper.cs
+++ Swabbr.Infrastructure/Notifications/JsonWrappers/FcmContentWrapper.cs
@@ -5,26 +5,4 @@
 /// </summary>
 internal sealed class FcmContentWrapper : NotificationWrapperJsonBase
 {
-    /// <summary>
-    ///     Subwrapper.
-    /// </summary>
-//     public FcmContentNotification Notification { get; set; }
 }
-
-/// <summary>
-///     Content wrapper json object template 
-///     for Apple Push Notification Services.
-/// </summary>
-internal sealed class FcmContentNotification
-{
-    /// <summary>
-    ///     APNS specific title.
-    /// </summary>
-    public string Title { get; set; }
-
-    /// <summary>
-    ///     APNS specific body.
-    /// </summary>
-    public string Body { get; set; }
-}
-
